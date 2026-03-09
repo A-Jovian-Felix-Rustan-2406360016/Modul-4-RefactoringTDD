895bc585bd165587c011344756434c87b3695c52
Jovian12 <infernum212@gmail.com>
2026-03-10 01:08:47 +0700

    [REFACTOR] Add enum payment status

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,31 +1,36 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 import java.util.Map;
-import java.util.Arrays;
-import java.util.List;
 
 @Getter
 public class Payment {
     private String id;
+    private Order order;
     private String method;
     private String status;
     private Map<String, String> paymentData;
 
-    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+    public Payment(String id, Order order, String method, Map<String, String> paymentData) {
         this.id = id;
-
-        if (method == null || method.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
+        this.order = order;
         this.method = method;
         this.paymentData = paymentData;
+
+        if (order == null || method == null || method.isEmpty() || paymentData == null || paymentData.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        this.status = PaymentStatus.WAITING_PAYMENT.getValue();
+    }
+
+    public Payment(String id, Order order, String method, String status, Map<String, String> paymentData) {
+        this(id, order, method, paymentData);
         this.setStatus(status);
     }
 
     public void setStatus(String status) {
-        List<String> validStatus = Arrays.asList("WAITING_PAYMENT", "SUCCESS", "REJECTED");
-        if (validStatus.contains(status)) {
+        if (PaymentStatus.contains(status)) {
             this.status = status;
         } else {
             throw new IllegalArgumentException();
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,54 +1,92 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentTest {
     private Map<String, String> paymentData;
+    private Order order;
 
     @BeforeEach
     void setUp() {
+        List<Product> products = new ArrayList<>();
+        Product product = new Product();
+        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(2);
+        products.add(product);
+
+        this.order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                products, 1708560000L, "Safira Sudrajat");
+
         this.paymentData = new HashMap<>();
         this.paymentData.put("voucherCode", "ESHOP1234ABC5678");
     }
 
     @Test
-    void testCreatePaymentWithValidData() {
-        Payment payment = new Payment("pay-1", "VOUCHER", "SUCCESS", paymentData);
+    void testCreatePaymentDefaultStatus() {
+        Payment payment = new Payment("pay-1", this.order, "VOUCHER", this.paymentData);
         assertEquals("pay-1", payment.getId());
+        assertSame(this.order, payment.getOrder());
         assertEquals("VOUCHER", payment.getMethod());
-        assertEquals("SUCCESS", payment.getStatus());
-        assertEquals(paymentData, payment.getPaymentData());
+        assertEquals(this.paymentData, payment.getPaymentData());
+        assertEquals(PaymentStatus.WAITING_PAYMENT.getValue(), payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentSuccessStatus() {
+        Payment payment = new Payment("pay-1", this.order, "VOUCHER",
+                PaymentStatus.SUCCESS.getValue(), this.paymentData);
+        assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentInvalidStatus() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new Payment("pay-1", "VOUCHER", "INVALID_STATUS", paymentData);
+            new Payment("pay-1", this.order, "VOUCHER", "MEOW", this.paymentData);
+        });
+    }
+
+    @Test
+    void testCreatePaymentNullOrder() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Payment("pay-1", null, "VOUCHER", this.paymentData);
         });
     }
 
     @Test
     void testCreatePaymentEmptyMethod() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new Payment("pay-1", "", "SUCCESS", paymentData);
+            new Payment("pay-1", this.order, "", this.paymentData);
+        });
+    }
+
+    @Test
+    void testCreatePaymentEmptyPaymentData() {
+        this.paymentData.clear();
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Payment("pay-1", this.order, "VOUCHER", this.paymentData);
         });
     }
 
     @Test
     void testSetStatusToRejected() {
-        Payment payment = new Payment("pay-1", "VOUCHER", "WAITING_PAYMENT", paymentData);
-        payment.setStatus("REJECTED");
-        assertEquals("REJECTED", payment.getStatus());
+        Payment payment = new Payment("pay-1", this.order, "VOUCHER", this.paymentData);
+        payment.setStatus(PaymentStatus.REJECTED.getValue());
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
     void testSetStatusToInvalidStatus() {
-        Payment payment = new Payment("pay-1", "VOUCHER", "SUCCESS", paymentData);
-        assertThrows(IllegalArgumentException.class, () -> payment.setStatus("MEOW"));
+        Payment payment = new Payment("pay-1", this.order, "VOUCHER", this.paymentData);
+        assertThrows(IllegalArgumentException.class, () -> payment.setStatus("BOOM"));
     }
 }
