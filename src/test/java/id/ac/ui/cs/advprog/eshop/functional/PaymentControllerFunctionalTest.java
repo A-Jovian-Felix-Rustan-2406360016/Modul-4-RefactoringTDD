38a67f82622c0e0140fbd074916dce117ee85dc9
Jovian12 <infernum212@gmail.com>
2026-03-10 12:35:32 +0700

    [CHORES] Add adminPaymentList and paymentDetail html

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/PaymentControllerFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/PaymentControllerFunctionalTest.java
@@ -1,10 +1,21 @@
 package id.ac.ui.cs.advprog.eshop.functional;
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,6 +28,33 @@ class PaymentControllerFunctionalTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @BeforeEach
+    void setUp() {
+        paymentRepository.findAll().clear();
+        List<Product> products = new ArrayList<>();
+        Product product = new Product();
+        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(10);
+        products.add(product);
+        Order dummyOrder = new Order(
+                "order-1",
+                products,
+                System.currentTimeMillis(),
+                "Jovian",
+                OrderStatus.WAITING_PAYMENT.getValue()
+        );
+
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP1234567890");
+
+        Payment dummyPayment = new Payment("pay-123", dummyOrder, "VOUCHER", paymentData);
+        paymentRepository.save(dummyPayment);
+    }
 
     @Test
     void testAdminPaymentListPageIsAccessible() throws Exception {
