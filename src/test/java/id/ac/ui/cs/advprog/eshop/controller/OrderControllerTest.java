db67431bea8f12ed5f7f3fb82c442eae17de8f1e
Jovian12 <infernum212@gmail.com>
2026-03-10 11:53:22 +0700

    [RED] Add functional test to order controller

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
@@ -0,0 +1,45 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(OrderController.class)
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService orderService;
+
+    @Test
+    void testCreateOrderPageStatus() throws Exception {
+        mockMvc.perform(get("/order/create"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order/createOrder"));
+    }
+
+    @Test
+    void testOrderHistoryPageStatus() throws Exception {
+        mockMvc.perform(get("/order/history"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order/orderHistory"));
+    }
+
+    @Test
+    void testShowOrderHistoryStatus() throws Exception {
+        mockMvc.perform(post("/order/history")
+                        .param("author", "Jovian"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order/orderList"))
+                .andExpect(model().attributeExists("orders"));
+    }
+}
